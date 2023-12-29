6648e6deed320d015c47b16f6a9c79248f754ac8
namkhanh2708 <146188914+namkhanh2708@users.noreply.github.com>
2023-12-29 18:12:50 +0700

    Create task 4.cpp

---
+++ cpp/task 4.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool isSafe(vector<string>& board, int row, int col, int n) {
+    // Check if there is a queen in the same column
+    for (int i = 0; i < row; i++) {
+        if (board[i][col] == 'Q') {
+            return false;
+        }
+    }
+
+    // Check if there is a queen in the upper left diagonal
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+        if (board[i][j] == 'Q') {
+            return false;
+        }
+    }
+
+    // Check if there is a queen in the upper right diagonal
+    for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
+        if (board[i][j] == 'Q') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void solveNQueens(vector<vector<string>>& solutions, vector<string>& board, int row, int n) {
+    if (row == n) {
+        solutions.push_back(board);
+        return;
+    }
+
+    for (int col = 0; col < n; col++) {
+        if (isSafe(board, row, col, n)) {
+            board[row][col] = 'Q';
+            solveNQueens(solutions, board, row + 1, n);
+            board[row][col] = '.';
+        }
+    }
+}
+
+vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> solutions;
+    vector<string> board(n, string(n, '.'));
+
+    solveNQueens(solutions, board, 0, n);
+
+    return solutions;
+}
+
+int main() {
+    int n = 4;
+    vector<vector<string>> solutions = solveNQueens(n);
+
+    for (const auto& solution : solutions) {
+        for (const auto& row : solution) {
+            cout << row << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
